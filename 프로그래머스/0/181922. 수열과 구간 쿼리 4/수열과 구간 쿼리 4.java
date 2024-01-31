2e766e7f45769defa066682ce0ac896b0ff3755a
breezekimm <155033650+breezekimm@users.noreply.github.com>
2024-01-31 16:03:33 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.01 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,20 +1,14 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = Arrays.copyOfRange(arr, 0, arr.length);
+        int[] answer = Arrays.copyOf(arr, arr.length);
 
         for (int[] query : queries) {
-            List<Integer> tempNums = new ArrayList<>();
             for (int i = query[0]; i <= query[1]; i++) {
                 if (i % query[2] == 0) {
-                    tempNums.add(i);
+                    answer[i]++;
                 }
-            }
-            for(int j = 0; j < tempNums.size(); j++) {
-                answer[tempNums.get(j)]++;
             }
         }
 
