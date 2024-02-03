2f94e5d1501f60f7edd95d5e2eadb155c510bca8
breezekimm <155033650+breezekimm@users.noreply.github.com>
2024-02-03 16:04:13 +0900

    [level 1] Title: 소수 찾기, Time: 33.18 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n) {
+        return countPrime(n);
+    }
+    
+    static int countPrime(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
