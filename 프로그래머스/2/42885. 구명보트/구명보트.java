3ec454d3531d8150cd1e1bf0c0f77ee4d5a8f166
breezekimm <155033650+breezekimm@users.noreply.github.com>
2024-07-15 16:32:59 +0900

    [level 2] Title: 구명보트, Time: 13.02 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int light = 0;
+        int heavy = people.length - 1;
+
+        Arrays.sort(people);
+
+        while (light <= heavy) {
+            if (people[light] + people[heavy] <= limit) {
+                light++;
+                heavy--;
+            } else {
+                heavy--;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
