b4888b13e309ed083cb92c673b8b37bd6bd74817
breezekimm <155033650+breezekimm@users.noreply.github.com>
2024-07-16 14:12:22 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.57 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,20 +4,20 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
 
-        PriorityQueue pq = new PriorityQueue();
+        PriorityQueue<Integer> pq = new PriorityQueue();
 
         for (int i = 0; i < score.length; i++) {
             if (i < k) {
                 // k일 까지는 명예의 전당 등록
                 pq.add(score[i]);
             } else {
-                int min = (int)pq.peek();
+                int min = pq.peek();
                 if (min < score[i]) {
-                    pq.poll();
-                    pq.add(score[i]);
+                    pq.poll();          // 최솟값 제거
+                    pq.add(score[i]);   // 새로운 점수를 명예의 전당 등록
                 }
             }
-                answer[i] = ((int)pq.peek());
+                answer[i] = pq.peek();
         }
         return answer;
     }
